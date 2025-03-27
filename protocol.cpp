b3569f20bfd21ecac8d9ea0e211df7f2017b3043
ysmreg <33682244+ysmreg@users.noreply.github.com>
2025-03-27 20:53:24 +0900

    process for rin

--- protocol.cpp
+++ protocol.cpp
@@ -24,7 +24,7 @@ static const char* ppszTypeName[] =
 
 unsigned short nDefaultP2Port = 0;
 
-unsigned char pchMessageStart[4] = { 0xf9, 0xbe, 0xb4, 0xd9 };
+unsigned char pchMessageStart[4] = { 0x52, 0x49, 0x4E, 0x43 };
 
 CMessageHeader::CMessageHeader()
 {
